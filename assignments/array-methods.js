0158d6c06d42aea0923a0ac1795aac2cec664115
Josh Schaben <jschaben42@gmail.com>
2019-10-04 18:01:20 -0500

    added JSON

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -72,7 +72,7 @@ console.log(firstNamesAllCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = runners.filter(people => people.shirt_size === "L");
-console.log(runnersLargeSizeShirt);
+console.log(JSON.stringify (runnersLargeSizeShirt, null, 2));
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
